4eec99fa0c569a003cd0617a8abf434dbfa69e86
dellonath <dellonath@gmail.com>
2022-05-22 17:23:30 -0300

    cokiecutter files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Analysis and Machine Learning applied in Educational Data such as Brazillian National Exam of High School.',
+    author='Douglas Oliveira',
+    license='MIT',
+)
