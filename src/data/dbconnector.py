e1a38b774d878113a397045824c5a3ceb57d40d2
dellonath <dellonath@gmail.com>
2022-05-30 18:31:06 -0300

    Change dbconnector.py directory

---
+++ src/data/dbconnector.py
@@ -0,0 +1,19 @@
+import sqlalchemy
+import pandas as pd
+
+class DatabaseConnection():
+    
+    '''
+    class just to connect with the database
+    '''
+    
+    def __init__(self, user = 'root', passw = 'mydbpass'):
+        self.cnx = sqlalchemy.create_engine(f'mysql+pymysql://{user}:{passw}@localhost/tfg')
+        
+    def select_query(self, query):
+        
+        connection = self.cnx.connect()
+        enem_candidates = pd.read_sql_query(query, connection)
+        connection.close()
+        
+        return enem_candidates
